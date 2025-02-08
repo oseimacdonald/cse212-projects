83b0e77b8eb4185031e052f9227d32363612e8f4
oseimacdonald <oseimacdonald@gmail.com>
2025-02-08 13:18:09 +0000

    week5

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Recursion
 {
@@ -14,8 +16,12 @@ public static class Recursion
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        // Base case: if n is less than or equal to 0, return 0
+        if (n <= 0)
+            return 0;
+    
+        // Recursive case: sum of squares of n and all numbers less than n
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -23,23 +29,26 @@ public static class Recursion
     /// # Problem 2 #
     /// #############
     /// Using recursion, insert permutations of length
-    /// 'size' from a list of 'letters' into the results list.  This function
-    /// should assume that each letter is unique (i.e. the 
+    /// 'size' from a list of 'letters' into the results list.  
+    /// This function should assume that each letter is unique (i.e. the 
     /// function does not need to find unique permutations).
-    ///
-    /// In mathematics, we can calculate the number of permutations
-    /// using the formula: len(letters)! / (len(letters) - size)!
-    ///
-    /// For example, if letters was [A,B,C] and size was 2 then
-    /// the following would the contents of the results array after the function ran: AB, AC, BA, BC, CA, CB (might be in 
-    /// a different order).
-    ///
-    /// You can assume that the size specified is always valid (between 1 
-    /// and the length of the letters list).
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        // Base case: if the word has reached the required size, add it to the results
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+    
+        // Recursive case: loop through each letter and generate the next permutation
+        for (int i = 0; i < letters.Length; i++)
+        {
+            // Exclude the letter already used and generate the next permutation
+            string remainingLetters = letters.Substring(0, i) + letters.Substring(i + 1);
+            PermutationsChoose(results, remainingLetters, size, word + letters[i]);
+        }
     }
 
     /// <summary>
@@ -48,44 +57,14 @@ public static class Recursion
     /// #############
     /// Imagine that there was a staircase with 's' stairs.  
     /// We want to count how many ways there are to climb 
-    /// the stairs.  If the person could only climb one 
-    /// stair at a time, then the total would be just one.  
-    /// However, if the person could choose to climb either 
-    /// one, two, or three stairs at a time (in any order), 
-    /// then the total possibilities become much more 
-    /// complicated.  If there were just three stairs,
-    /// the possible ways to climb would be four as follows:
-    ///
-    ///     1 step, 1 step, 1 step
-    ///     1 step, 2 step
-    ///     2 step, 1 step
-    ///     3 step
-    ///
-    /// With just one step to go, the ways to get
-    /// to the top of 's' stairs is to either:
-    ///
-    /// - take a single step from the second to last step, 
-    /// - take a double step from the third to last step, 
-    /// - take a triple step from the fourth to last step
-    ///
-    /// We don't need to think about scenarios like taking two 
-    /// single steps from the third to last step because this
-    /// is already part of the first scenario (taking a single
-    /// step from the second to last step).
-    ///
-    /// These final leaps give us a sum:
-    ///
-    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
-    ///                       CountWaysToClimb(s-2) +
-    ///                       CountWaysToClimb(s-3)
-    ///
-    /// To run this function for larger values of 's', you will need
-    /// to update this function to use memoization.  The parameter
-    /// 'remember' has already been added as an input parameter to 
-    /// the function for you to complete this task.
+    /// the stairs, where you can take 1, 2, or 3 steps at a time.  
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
+        // Initialize the dictionary if it's null
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
+
         // Base Cases
         if (s == 0)
             return 0;
@@ -96,10 +75,17 @@ public static class Recursion
         if (s == 3)
             return 4;
 
-        // TODO Start Problem 3
+        // Check if the result is already memoized
+        if (remember.ContainsKey(s))
+            return remember[s];
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        // Recursive case: the number of ways to reach step s is the sum of the ways
+        // to reach step s-1, step s-2, and step s-3
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+
+        // Memoize the result
+        remember[s] = ways;
+    
         return ways;
     }
 
@@ -107,23 +93,37 @@ public static class Recursion
     /// #############
     /// # Problem 4 #
     /// #############
-    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
-    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
-    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
-    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
-    /// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
-    ///	
-    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
-    /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
+    /// A binary string is a string consisting of just 1's and 0's.  
+    /// A wildcard '*' represents multiple binary string possibilities.
+    /// This function generates all binary strings that match the pattern.
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        // Base case: If there's no '*' in the pattern, add it to results
+        if (!pattern.Contains("*"))
+        {
+            results.Add(pattern);
+            return;
+        }
+        
+        // Recursive case: Find the first '*' in the pattern
+        int starIndex = pattern.IndexOf("*");
+        
+        // Two recursive calls: one replacing '*' with '0' and the other with '1'
+        string patternWithZero = pattern.Substring(0, starIndex) + '0' + pattern.Substring(starIndex + 1);
+        string patternWithOne = pattern.Substring(0, starIndex) + '1' + pattern.Substring(starIndex + 1);
+        
+        // Recursively generate patterns for both modified strings
+        WildcardBinary(patternWithZero, results);
+        WildcardBinary(patternWithOne, results);
     }
 
     /// <summary>
-    /// Use recursion to insert all paths that start at (0,0) and end at the
-    /// 'end' square into the results list.
+    /// #############
+    /// # Problem 5 #
+    /// #############
+    /// Using recursion to insert all paths from (0,0) to the 'end' square 
+    /// of the maze into the results list.
     /// </summary>
     public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
     {
@@ -141,3 +141,4 @@ public static class Recursion
         // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
 }
+
